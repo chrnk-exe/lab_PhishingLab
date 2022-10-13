7c426d695122b54b2c39b5bc73df9fc51b2d16b3
chrnk-exe <ivan_kot2001@mail.ru>
2022-10-13 18:20:51 +0300

    recloneapp

--- deploy/reclone-app.sh
+++ deploy/reclone-app.sh
@@ -1,9 +1,11 @@
 rm -rf App
 docker rmi -f dslab
+sleep 1
+docker rm $(docker ps -a -f status=exited)
 # download app
 mkdir -p App
 git clone https://github.com/chrnk-exe/lab_PhishingLab ./App
 cd App/deploy
 # run docker-compose.yml
 sudo docker build -t dslab .
-sudo docker run -p 80:80 
+sudo docker run -p 80:80 dslab
