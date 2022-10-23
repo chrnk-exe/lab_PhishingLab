152385104130a4a35895c76f6d2d22a84ffdca9c
chrnk-exe <ivan_kot2001@mail.ru>
2022-10-23 16:05:48 +0300

    messagecountfix

--- src/client/src/components/MessageCount.tsx
+++ src/client/src/components/MessageCount.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const MessageCount = ({ messages }: { messages: Message[] }) => (
 	<div>
-		{messages.filter(message => !message.read && message.type === 'inbox').length} / {messages.length}
+		{
+			messages.filter(
+				message => !message.read && message.type === 'inbox',
+			).length
+		}{' '}
+		/{' '}
+		{
+			messages.filter(
+				message => message.type === 'inbox',
+			).length
+		}
 	</div>
 );
 
